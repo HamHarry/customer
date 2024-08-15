c2b671ef7a5221f202b49e50b887d5f0265ae803
HamHarry <hamza_2641@hotmail.com>
2024-08-15 15:06:58 +0700

    ลองทำเอง และปรับความเข้าใจ

--- src/App.tsx
+++ src/App.tsx
@@ -13,20 +13,41 @@ export interface User {
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
 
-  const fetchUsersData = useCallback(async () => {
+  const fetchUsers = useCallback(async () => {
     const res = await axios.get<User[]>("https://www.melivecode.com/api/users");
     const usersData = res.data;
     setUsers(usersData);
   }, []);
   useEffect(() => {
-    fetchUsersData();
-  }, [fetchUsersData]);
+    fetchUsers();
+  }, [fetchUsers]);
 
   return (
     <div>
-      {users.map((item, index) => {
-        return <div key={index}>{item.fname}</div>;
-      })}
+      <table>
+        <thead>
+          <th>Id</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>username</th>
+          <th>avatar</th>
+        </thead>
+        <tbody>
+          {users.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{item.id}</td>
+                <td>{item.fname}</td>
+                <td>{item.lname}</td>
+                <td>{item.username}</td>
+                <td>
+                  <img src={item.avatar} alt="" />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
