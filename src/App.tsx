91b3aeab0d24c1967f60e3a99c36a2a842a35840
HamHarry <hamza_2641@hotmail.com>
2024-08-15 23:14:26 +0700

    search

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,40 @@ export interface User {
   id: number;
   fname: string;
   lname: string;
-  username: string;
+  username?: string;
   avatar: string;
 }
 
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [search, setSearch] = useState<string>("");
 
   const fetchUsers = useCallback(async () => {
-    const res = await axios.get<User[]>("https://www.melivecode.com/api/users");
+    const res = await axios.get("https://www.melivecode.com/api/users");
     const usersData = res.data;
+
     setUsers(usersData);
   }, []);
+
   useEffect(() => {
     fetchUsers();
   }, [fetchUsers]);
 
+  const fetchUserBySearch = useCallback(async () => {
+    const res = await axios.get<User[]>(
+      `https://www.melivecode.com/api/users?search=${search}`
+    );
+    const usersDataBySearch = res.data;
+    setUsers(usersDataBySearch);
+  }, [search]);
+
+  useEffect(() => {
+    fetchUserBySearch();
+  }, [fetchUserBySearch]);
+
   return (
     <div>
+      <input type="text" onChange={(e) => setSearch(e.target.value)} />
       <table>
         <thead>
           <th>Id</th>
@@ -39,7 +55,7 @@ const App = () => {
                 <td>{item.id}</td>
                 <td>{item.fname}</td>
                 <td>{item.lname}</td>
-                <td>{item.username}</td>
+                <td>{item?.username}</td>
                 <td>
                   <img src={item.avatar} alt="" />
                 </td>
